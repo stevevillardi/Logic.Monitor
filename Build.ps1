99a91d2bd28efbe1666f636f8145c800c1d3f018
Steve Villardi <steven.villardi@logicmonitor.com>
2022-12-13 21:59:32 -0500

    CI pipeline config

---
+++ Build.ps1
@@ -0,0 +1,27 @@
+$Global:ErrorActionPreference = 'Stop'
+$Global:VerbosePreference = 'SilentlyContinue'
+
+$buildVersion = $env:BUILD_VERSION
+$manifestPath = "./Logic.Monitor.psd1"
+$publicFuncFolderPath = './Public'
+
+
+if (!(Get-PackageProvider | Where-Object { $_.Name -eq 'NuGet' })) {
+    Install-PackageProvider -Name NuGet -force | Out-Null
+}
+Import-PackageProvider -Name NuGet -force | Out-Null
+
+if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+}
+
+$manifestContent = (Get-Content -Path $manifestPath -Raw) -replace '<ModuleVersion>', $buildVersion
+
+if ((Test-Path -Path $publicFuncFolderPath) -and ($publicFunctionNames = Get-ChildItem -Path $publicFuncFolderPath -Filter '*.ps1' | Select-Object -ExpandProperty BaseName)) {
+    $funcStrings = "'$($publicFunctionNames -join "','")'"
+} else {
+    $funcStrings = $null
+}
+
+$manifestContent = $manifestContent -replace "'<FunctionsToExport>'", $funcStrings
+$manifestContent | Set-Content -Path $manifestPath
