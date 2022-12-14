278b1dbaf98fd57392105313282684e6c4a83c25
Steve Villardi <steven.villardi@logicmonitor.com>
2022-12-13 22:23:25 -0500

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -15,6 +15,16 @@ if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 }
 
+if(!(Get-Module PSWriteHTML -ListAvailable)){
+    Install-Module PSWriteHTML -Force -Confirm:$false
+}
+if(!(Get-Module Microsoft.PowerShell.SecretManagement -ListAvailable)){
+    Install-Module Microsoft.PowerShell.SecretManagement -Force -Confirm:$false
+}
+if(!(Get-Module Microsoft.PowerShell.SecretStore -ListAvailable)){
+    Install-Module Microsoft.PowerShell.SecretStore -Force -Confirm:$false
+}
+
 $manifestContent = (Get-Content -Path $manifestPath -Raw) -replace '<ModuleVersion>', $buildVersion
 
 if ((Test-Path -Path $publicFuncFolderPath) -and ($publicFunctionNames = Get-ChildItem -Path $publicFuncFolderPath -Filter '*.ps1' | Select-Object -ExpandProperty BaseName)) {
